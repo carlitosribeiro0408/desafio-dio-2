4f9afa1e99f260aadfb215b8f93bc21a413a0828
carlitosribeiro0408 <isaac.oliveira0408@outlook.com>
2023-11-22 00:21:18 -0300

    Revisado com chatgpt e ajustado por mim

--- calc.py
+++ calc.py
@@ -1,41 +1,43 @@
 def nivel(level):
-    rank={
-        1:"Ferro",
-        2:"Bronze",
-        3:"Prata",
-        4:"Ouro",
-        5:"Diamante",
-        6:"Lendário",
-        7:"Imortal"
+    ranks = {
+        1: "Ferro",
+        2: "Bronze",
+        3: "Prata",
+        4: "Ouro",
+        5: "Diamante",
+        6: "Lendário",
+        7: "Imortal"
     }
-    print(f"e está no nivel {rank.get(level)}")
+    return ranks.get(level)
 
 def ratio(win, loss):
     return win - loss
 
-w = int(input("Digite o número de vitórias:"))
-l = int(input("Digite o número de derrotas:"))
+# Obter entradas do usuário com validação
+try:
+    w = int(input("Digite o número de vitórias: "))
+    l = int(input("Digite o número de derrotas: "))
+except ValueError:
+    print("Por favor, insira números inteiros válidos para vitórias e derrotas.")
+    exit()
 
-saldo = ratio(w,l)
+saldo = ratio(w, l)
 
+# Simplificar condições
 if saldo < 10:
-    print("O herói tem de saldo", saldo, end=" ")
-    nivel(1)
-elif saldo > 10 < 20:
-    print("O herói tem de saldo", saldo, end=" ")
-    nivel(2)
-elif saldo > 20 < 50:
-    print("O herói tem de saldo", saldo, end=" ")
-    nivel(3)
-elif saldo > 50 < 80:
-    print("O herói tem de saldo", saldo, end=" ")
-    nivel(4)
-elif saldo > 80 < 90:
-    print("O herói tem de saldo", saldo, end=" ")
-    nivel(5)
-elif saldo > 90 < 100:
-    print("O herói tem de saldo", saldo, end=" ")
-    nivel(6)
-elif saldo >= 101:
-    print("O herói tem de saldo", saldo, end=" ")
-    nivel(7)
+    level = 1
+elif 10 <= saldo < 20:
+    level = 2
+elif 20 <= saldo < 50:
+    level = 3
+elif 50 <= saldo < 80:
+    level = 4
+elif 80 <= saldo < 90:
+    level = 5
+elif 90 <= saldo < 100:
+    level = 6
+else:
+    level = 7
+
+# Mensagem de saída formatada
+print(f"O herói tem um saldo de {saldo} e está no nível {nivel(level)}.")
